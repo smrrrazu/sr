51486a06b0f213cc16816257059bf85a2ed6b23b
Sheikh Razu <sra@SRA-MacBook-2.local>
2016-06-15 17:01:09 +0200

    ddfdf

--- sr/onlineshop/views.py
+++ sr/onlineshop/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from api.models import Category, Product
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    
+    categories = Category.objects.all()
+
+    categories = Category.objects.order_by('-created')[:5]
+    template = loader.get_template("onlineshop/index.html")
+    context = {
+        'categories': categories
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def add(request):
+    return HttpResponse("Add ")
